10fece21d844e4a03c3bb8af8636a864ebc3dd1a
Karman Singh <karmansingh@icloud.com>
2020-01-24 16:55:18 -0500

    String comments

--- part2/string.h
+++ part2/string.h
@@ -4,40 +4,60 @@
 #include <string.h>
 #include <cassert>
 
-// Represents a String class implementation.
+/**
+ * Object: this class represents a string of characters
+ * 
+ * authors: singh.kar@husky.neu.edu and haiber.ow@husky.neu.edu
+ */
 class String : public Object
 {
 public:
-    // Value of the String
-    char *val;
-    int size_;
+    char *val; // value of the String 
+    int size_; // length of the String
 
-    // Constructor to make Strings
+    /**
+     * Creates a String given an array of characters.
+     * 
+     * @arg input  the value of the String that is being created
+     */
     String(char *input)
     {
         val = input;
         size_ = strlen(val);
     }
 
-    // Constructor to make Strings
+    /**
+     * Creates a String given an array of characters.
+     * 
+     * @arg input  the value of the String that is being created
+     */
     String(const char *input)
     {
         val = duplicate(input);
         size_ = strlen(val);
     }
 
+    /**
+     * Destructor to delete the value of this String. 
+     */
     ~String()
     {
         delete val;
     }
 
-    // Returns the value of this String.
+    /**
+     * Returns the value of this String in char* form. 
+     */
     char *getValue()
     {
         return val;
     }
 
-    // Concatenates this String with the other one.
+    /**
+     * Concatenates this String with the other one.
+     * 
+     * @arg other  the String being appended to the end of this one
+     */
     String *concat(String *other)
     {
         size_t buffersize = 0;
@@ -52,7 +72,11 @@ public:
         return new String(buffer);
     }
 
-    // Checks to see if this String is equal to the other one.
+    /**
+     * Checks to see if this String is equal to the other one.
+     * 
+     * @arg other  the String being compared against
+     */
     bool equals(String *other)
     {
         char *str1 = val;
@@ -74,6 +98,11 @@ public:
         delete str2;
     }
 
+    /**
+     * Converts a const char* into a char*
+     * 
+     * @arg s  the const char* to be converted
+     */
     char *duplicate(const char *s)
     {
         char *res = new char[strlen(s) + 1];
