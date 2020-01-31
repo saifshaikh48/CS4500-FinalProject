d1b38784b402fe80bc36a85477f90a9b24a66b8e
Karman Singh <karmansingh@icloud.com>
2020-01-30 23:23:53 -0500

    String class update

--- part2/string.h
+++ part2/string.h
@@ -1,111 +1,49 @@
+//lang::CwC
 #pragma once
+
 #include "object.h"
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio> 
 
 /**
- * Object: this class represents a string of characters
- * 
- * authors: singh.kar@husky.neu.edu and haiber.ow@husky.neu.edu
- */
-class String : public Object
-{
+ * An immutable String class representing a char*
+ * author: chasebish */
+class String : public Object {
 public:
-    char *val; // value of the String
-    int size_; // length of the String
+  /** CONSTRUCTORS & DESTRUCTORS **/
 
-    /**
-     * Creates a String given an array of characters.
-     * 
-     * @arg input  the value of the String that is being created
-     */
-    String(char *input)
-    {
-        val = input;
-        size_ = strlen(val);
-    }
+  /* Creates a String copying s */
+  String(const char* s);
 
-    /**
-     * Creates a String given an array of characters.
-     * 
-     * @arg input  the value of the String that is being created
-     */
-    String(const char *input)
-    {
-        val = duplicate(input);
-        size_ = strlen(val);
-    }
+  /* Copies a String copying the value from s */
+  String(String* const s);
 
-    /**
-     * Destructor to delete the value of this String. 
-     */
-    ~String()
-    {
-        delete val;
-    }
+  /* Clears String from memory */
+  ~String();
 
-    /**
-     * Returns the value of this String in char* form. 
-     */
-    char *getValue()
-    {
-        return val;
-    }
 
-    /**
-     * Concatenates this String with the other one.
-     * 
-     * @arg other  the String being appended to the end of this one
-     */
-    String *concat(String *other)
-    {
-        size_t buffersize = 0;
+  /** INHERITED METHODS **/
 
-        buffersize = buffersize + strlen(val) + strlen(other->getValue());
+  /* Inherited from Object, generates a hash for a String */
+  size_t hash();
 
-        char *buffer = new char[buffersize];
+  /* Inherited from Object, checks equality between an String and an Object */
+  bool equals(Object* const obj);
 
-        strcpy(buffer, val);
-        strcat(buffer, other->getValue());
 
-        return new String(buffer);
-    }
+  /** STRING METHODS **/
+  
+  /** Compares strings based on alphabetical order
+   * < 0 -> this String is less than String s
+   * = 0 -> this String is equal to String s
+   * > 0 -> this String is greater than String s
+   */
+  int cmp(String* const s);
 
-    /**
-     * Checks to see if this String is equal to the other one.
-     * 
-     * @arg other  the String being compared against
-     */
-    bool equals(String *other)
-    {
-        char *str1 = val;
-        char *str2 = other->getValue();
+  /* Creates a new String by combining two existing Strings */
+  String* concat(String* const s);
 
-        int result;
-        result = strcmp(str1, str2);
-        if (result == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-        // Don't need the local variables anymore.
-        delete str1;
-        delete str2;
-    }
-
-    /**
-     * Converts a const char* into a char*
-     * 
-     * @arg s  the const char* to be converted
-     */
-    char *duplicate(const char *s)
-    {
-        char *res = new char[strlen(s) + 1];
-        strcpy(res, s);
-        return res;
-    }
+  /* Returns the current length of the String */
+  size_t size();
 };
